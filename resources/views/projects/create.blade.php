6a7de3967bb4b236d472753ac663e87caea9f12f
Cosmin Stanjic <cosmin.stanjic@amdaris.com>
2021-02-16 20:27:46 +0200

    add markup and styles for show project page

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,16 +1,9 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <form method="POST" action="/projects" class="container pt-3 text-uppercase">
+@extends('layouts.app')
+
+@section('content')
+    <form method="POST" action="/projects">
         @csrf
-        <h1>Create a Project</h1>
+        <h1 class="text-uppercase">Create a Project</h1>
 
         <div class="form-group">
             <label for="title">Title</label>
@@ -21,6 +14,6 @@
             <textarea type="text" name="description" id="description" class="form-control" placeholder="Description"></textarea>
         </div>
         <button type="submit" class="btn btn-primary">Create Project</button>
+        <a href="/projects">Cancel</a>
     </form>
-</body>
-</html>
+@endsection
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,13 +1,44 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>{{ $project->title }}</h1>
-    <div>{{ $project->description }}</div>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <header class="d-flex align-items-center mb-3">
+        <div class="d-flex justify-content-between align-items-center w-100">
+            <p class="text-muted mt-3">
+                <a href="/projects">My Projects</a> / {{ $project->title }}
+            </p>
+            <a href="/projects/create" class="btn btn-info text-white">New Project</a>
+        </div>
+    </header>
+
+    <main>
+        <div class="row">
+            <div class="col-md-9 px-3 mb-4">
+                <div class="mb-5">
+                    <h2 class="text-muted mb-2">Tasks</h2>
+
+                    <div class="bg-white px-3 py-4 rounded shadow px-3 mb-2">Lorem ipsum</div>
+                    <div class="bg-white px-3 py-4 rounded shadow px-3 mb-2">Lorem ipsum</div>
+                    <div class="bg-white px-3 py-4 rounded shadow px-3 mb-2">Lorem ipsum</div>
+                    <div class="bg-white px-3 py-4 rounded shadow px-3 mb-2">Lorem ipsum</div>
+                </div>
+                <div>
+                    <h2 class="text-muted mb-2">General Notes</h2>
+
+                    <textarea class="bg-white px-3 py-4 rounded shadow w-100" style="height: 200px">Lorem ipsum</textarea>
+                </div>
+            </div>
+
+            <div class="col-md-3 py-3">
+                <div class="bg-white px-3 py-4 mt-4 rounded shadow">
+                    <h3 class="mb-3 title">
+                        <a href="{{ $project->path() }}" class="text-dark">{{ $project->title }}</a>
+                    </h3>
+
+                    <div class="text-muted">{{ \Illuminate\Support\Str::limit($project->description, 100) }}</div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+@endsection
